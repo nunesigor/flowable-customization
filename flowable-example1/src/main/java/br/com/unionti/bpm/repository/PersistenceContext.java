df0ac471a9d8b572c2e244948df5ad52338f4ee7
aberafael <40636932+aberafael@users.noreply.github.com>
2018-07-02 23:26:11 -0300

    Classe alternativa ao JPASpreingTest-context.xml

---
+++ flowable-example1/src/main/java/br/com/unionti/bpm/repository/PersistenceContext.java
@@ -0,0 +1,63 @@
+package br.com.unionti.bpm.repository;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@EnableJpaRepositories(basePackages = {
+        "br.com.unionti.bpm.repository"
+})
+@EnableTransactionManagement
+public class PersistenceContext {
+
+	@Bean(destroyMethod = "close")
+    DataSource dataSource(Environment env) {
+        HikariConfig dataSourceConfig = new HikariConfig();
+        dataSourceConfig.setDriverClassName(env.getRequiredProperty("spring.datasource.driver-class-name"));
+        dataSourceConfig.setJdbcUrl(env.getRequiredProperty("spring.datasource.url"));
+        dataSourceConfig.setUsername(env.getRequiredProperty("spring.datasource.username"));
+        dataSourceConfig.setPassword(env.getRequiredProperty("spring.datasource.password"));
+ 
+        return new HikariDataSource(dataSourceConfig);
+    }
+	
+	@Bean
+    LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, 
+                                                                Environment env) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactoryBean.setPackagesToScan("br.com.unionti.bpm");
+ 
+        Properties jpaProperties = new Properties();
+     
+        jpaProperties.put("hibernate.hbm2ddl.auto", 
+                env.getRequiredProperty("spring.jpa.hibernate.ddl-auto")
+        );
+
+        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+ 
+        return entityManagerFactoryBean;
+    }
+	
+	@Bean
+    JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+}
