f1203ff915b5f2a45c1fc3485d5fd8dcd70e2e30
Igor Nunes <inunes@identropy.com>
2018-07-11 20:48:19 -0300

    login parcial

--- union-bpm-app/src/app/app.module.ts
+++ union-bpm-app/src/app/app.module.ts
@@ -1,20 +1,62 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injectable } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app.routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { HomeComponent } from './home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserXhr, BaseRequestOptions, RequestOptions } from '@angular/http';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
+@Injectable()
+export class CustomBrowserXhr extends BrowserXhr {
+  constructor() {
+    super();
+  }
+
+  build(): any {
+    let xhr = super.build();
+    xhr.withCredentials = true;
+    return <any>(xhr);
+  }
+}
+
+export class CustomRequestOptions extends BaseRequestOptions {
+  constructor() {
+    super();
+    if (!this.headers.has('XMLHttpRequest')) {
+      this.headers.append('X-Requested-With', 'XMLHttpRequest');
+    }
+    if (!this.headers.has('application/json')) {
+      this.headers.append('Content-type', 'application/json');
+    }
+  }
+}
 
 @NgModule({
   declarations: [
+    //components here
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    //services and guards here
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    { provide: RequestOptions, useClass: CustomRequestOptions },
+    { provide: BrowserXhr, useClass: CustomBrowserXhr },
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- union-bpm-app/src/environments/environment.ts
+++ union-bpm-app/src/environments/environment.ts
@@ -3,7 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  //VERSION: require("../../package.json").version,
+  production: false,
+  anonymous_user: 'anonymous',
+  anonymous_password: 'anonymous',
+  //for deploy - script will change the %%TOMCAT_HOST%%
+  //idm_app: 'https://%%TOMCAT_HOST%%/flowable-idm/',
+  //task_app: 'https://%%TOMCAT_HOST%%/flowable-task/',
+  //admin_app: 'https://%%TOMCAT_HOST%%/flowable-admin/',
+  //rest_app: 'https://%%TOMCAT_HOST%%/flowable-rest/'
+
+  //for development
+  idm_app: 'http://localhost:8080/flowable-idm/',
+  task_app: 'https://localhost:8080/flowable-task/',
+  admin_app: 'https://localhost:8080/flowable-admin/',
+  rest_app: 'https://localhost:8080/flowable-rest/'
+
 };
 
 /*
